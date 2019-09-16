93c177940a01d6c7f9e21539dbb159207c016da5
PabloMaster2 <55392265+PabloMaster2@users.noreply.github.com>
2019-09-16 14:28:26 +0200

    Add files via upload

---
+++ Try.php
@@ -0,0 +1,12 @@
+$foo = 10;
+ 
+try {
+    if( is_numeric($foo) ){
+        if ($foo > 5){
+            throw new Exception('number too high'); // this will be thrown
+        }
+    }
+    else {
+        throw new Exception('not a number type');
+    }
+}
